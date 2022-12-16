2d5207d1de877a5dc78e630d3b3a748c521c9c83
Kaio Maciel <43120150+kaiogotyacode@users.noreply.github.com>
2022-12-16 11:51:03 -0300

    Add project files.

---
+++ CS-SaideraFatec/Controllers/RegisterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CS_SaideraFatec.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Signup()
+        {
+            return View();
+        }
+    }
+}
