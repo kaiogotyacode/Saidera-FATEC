2995b7190982a611c69879f335252754403de549
Kaio Maciel <43120150+kaiogotyacode@users.noreply.github.com>
2022-12-16 12:41:40 -0300

    Removendo duplicidade

--- CS-SaideraFatec/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CS_SaideraFatec.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
